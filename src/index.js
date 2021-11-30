5239c26f6e2c25bb29d21b74708f18559d4bdc1d
Jorge Evangelista <jorgeevangelista500@gmail.com>
2021-11-30 18:42:15 -0500

    finisehd the reiminder of the eventlistener

--- src/index.js
+++ src/index.js
@@ -31,8 +31,55 @@ const busPic = document.querySelector(".intro img")
 // console.log(busPic)
 busPic.addEventListener("wheel", zoom);
 
-const buttons = document.querySelector(".btn")
-buttons.addEventListener("dblclick", function(event){
+const button = document.querySelector(".btn")
+button.addEventListener("dblclick", function(event){
     event.target.style.backgroundColor = "red"
 })
     
+const welcome = document.querySelector(".intro p")
+console.log(welcome)
+welcome.addEventListener("select", function(event){
+   event.target.style.color = "green"
+})
+
+// const letGo = document.querySelector(".content-section h2")
+
+window.addEventListener("keydown", function(event){
+    if  (event.key === "Escape")
+    event.target.style.color = "red"
+})
+
+const destinationsTitle = document.querySelector(".destination h4")
+console.log(destinationsTitle)
+
+destinationsTitle.addEventListener("copy", function(event){
+   event.target.style.color = "green"
+})
+
+document.addEventListener("fullscreenchange", function(event){
+    event.target.style.color = "red"
+})
+
+button.addEventListener("auxclick", function(event){
+    event.target.style.color = "green"
+})
+
+const footer = document.querySelector(".footer")
+footer.addEventListener("mouseout", function(event) {
+    event.target.style.color = "purple";
+    setTimeout(function() {
+    event.target.style.color = "";
+      }, 400);
+    }, false);
+
+    links.forEach (link => 
+        link.addEventListener("mouseleave", function(event){
+            event.target.style.color = "orange";
+
+            setTimeout(function() {
+                event.target.style.color = "";
+              }, 1000);
+
+            }, false))
+
+        
