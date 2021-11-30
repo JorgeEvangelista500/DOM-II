1def4c21fb4eb74a9f014b5dbcfd3e8b8f004aee
Jorge Evangelista <jorgeevangelista500@gmail.com>
2021-11-30 15:43:14 -0500

    finisehd first two

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './less/index.less'
 
 // Your code goes here!
+const links = document.querySelectorAll("a");
+links.forEach (link => {
+    link.addEventListener("click", function(event){
+        event.preventDefault();
+    })
+})
+
+const logo = document.querySelector(".logo-heading")
+console.log(logo)
+logo.addEventListener("mouseover", function(event){
+    event.target.style.color = "blue"
+})
